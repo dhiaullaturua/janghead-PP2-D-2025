432c274cd3d82bb2d4dc18de12bad5b9161c74ef
dhiaullaturua <146175041+dhiaullaturua@users.noreply.github.com>
2025-12-30 18:06:02 +0700

    Update koneksi.java

--- config/koneksi.java
+++ config/koneksi.java
@@ -8,25 +8,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author dhiaulhaqlaturua
- */
+
 public class koneksi {
     
      private static Connection koneksi;
 
-    public static Connection getKoneksi() {
-        try {
-            if (koneksi == null) {
-                String url = "jdbc:mysql://localhost:3306/Manajemen_Data_Karyawan";
-                String user = "root";
-                String pass = "";
-                koneksi = DriverManager.getConnection(url, user, pass);
-            }
-        } catch (SQLException e) {
-            System.out.println("Koneksi Gagal: " + e.getMessage());
-        }
-        return koneksi;
-    }
+    
 }
