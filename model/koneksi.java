751445d0ff59961d16607bde14ba3b974d3a8def
dhiaullaturua <146175041+dhiaullaturua@users.noreply.github.com>
2026-01-02 21:31:11 +0700

    Update koneksi.java

--- model/koneksi.java
+++ model/koneksi.java
@@ -2,31 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package config;
+package ;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-/**
- *
- * @author dhiaulhaqlaturua
- */
 public class koneksi {
     
-     private static Connection koneksi;
-
-    public static Connection getKoneksi() {
-        try {
-            if (koneksi == null) {
-                String url = "jdbc:mysql://localhost:3306/Manajemen_Data_Karyawan";
-                String user = "root";
-                String pass = "";
-                koneksi = DriverManager.getConnection(url, user, pass);
-            }
-        } catch (SQLException e) {
-            System.out.println("Koneksi Gagal: " + e.getMessage());
-        }
-        return koneksi;
-    }
+    
 }
