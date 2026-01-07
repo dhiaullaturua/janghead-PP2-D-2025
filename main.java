675eb1d83b17beeb7a93efc577b08f54980d3407
dhiaullaturua <146175041+dhiaullaturua@users.noreply.github.com>
2026-01-08 00:43:51 +0700

    feat: Menambahkan main.java

---
+++ main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import view.mainApp_bankrut;
+import javax.swing.SwingUtilities;
+
+
+/**
+ *
+ * @author dhiaulhaqlaturua
+ */
+public class main {
+    
+     public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new mainApp_bankrut().setVisible(true));
+    }
+
+    
+}
